c9a5ccd6aeba777621a7ffb0a7cc636692af0c3e
JMD <jmdagenais@hotmail.ca>
2015-12-26 17:35:29 -0500

    first commit

---
+++ app/templates/templates.js
@@ -0,0 +1,15 @@
+'use strict'
+
+angular.module('templateStore.templates', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+   .when('/templates', {
+      templateUrl: 'templates/templates.html',
+      controller: 'TemplatesCtrl'
+   });
+}])
+
+.controller('TemplatesCtrl', ['$scope', function($scope){
+  console.log($scope);
+}]);
