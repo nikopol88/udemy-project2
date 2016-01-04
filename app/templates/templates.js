f78e229ac2d34f56755a06d0574bd83d06fbd00f
JMD <jmdagenais@hotmail.ca>
2016-01-03 19:23:22 -0500

    change templates view and add route for template details

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -1,15 +1,21 @@
-'use strict'
-
-angular.module('templateStore.templates', ['ngRoute'])
+angular.module('templateStore.templates',['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider){
-  $routeProvider
-   .when('/templates', {
-      templateUrl: 'templates/templates.html',
-      controller: 'TemplatesCtrl'
-   });
+	$routeProvider.
+		when('/templates', {
+			templateUrl: 'templates/templates.html',
+			controller: 'TemplatesCtrl'
+		}).
+		when('/templates/:templateId', {
+			templateUrl: 'templates/template-details.html',
+			controller: 'TemplateDetailsCtrl'
+		})
 }])
 
 .controller('TemplatesCtrl', ['$scope', function($scope){
-  console.log($scope);
+
+}])
+
+.controller('TemplateDetailsCtrl', ['$scope', function($scope){
+	
 }]);
